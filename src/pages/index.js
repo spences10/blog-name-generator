f051dd1c0716a2d7d30e1fcc74c269a7f9deeaae
spences10 <spences10apps@gmail.com>
2020-12-25 18:08:24 +0000

    add box variants

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,21 @@ export default function Home() {
   const [blogName, blogNameSet] = useState(`Your Blog Name here!!`)
   return (
     <Box as="div">
-      <h1>Blog Name Generator</h1>
-      <h2>{blogName}</h2>
-      <button
+      <Box as="h1" variant="styles.h1">
+        Blog Name Generator
+      </Box>
+      <Box as="h2" variant="styles.h2">
+        {blogName}
+      </Box>
+      <Box
+        as="button"
+        variant="styles.button"
         onClick={() => {
           blogNameSet(getBlogName())
         }}
       >
         Get Name
-      </button>
+      </Box>
     </Box>
   )
 }
--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,15 @@
 export default {
-  styles: { p: { color: '#639' } },
+  styles: {
+    p: { color: '#639' },
+    button: {
+      marginTop: '5rem',
+      padding: '3rem 6rem',
+      border: 'none',
+      backgroundImage:
+        'linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)',
+      borderRadius: '30px',
+      fontFamily: 'Radley',
+      fontSize: '2rem',
+    },
+  },
 }
